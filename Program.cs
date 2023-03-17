d430b3bf3bccf85ff5b4db3831d16caa317a4d2c
cinvenz <vincenzobortune@gmail.com>
2023-03-17 17:05:27 +0100

    insert second bonus

--- Program.cs
+++ Program.cs
@@ -1,72 +1,96 @@
 ﻿//var numbers = new int[] { 2, 6, 7, 5, 3, 9 };
 
-Console.WriteLine("Quanti numeri vuoi inserire?");
-int insertNum = int.Parse(Console.ReadLine());
+//Console.WriteLine("Quanti numeri vuoi inserire?");
+//int insertNum = int.Parse(Console.ReadLine());
 
-int[] numbers = new int[insertNum];
-for (int i = 0; i < insertNum; i++)
-{
-    Console.WriteLine($"Inserisci il {i + 1}° numero:");
-    numbers[i] = int.Parse(Console.ReadLine());
-    int quadrato = Quadrato(numbers[i]);
-    Console.WriteLine($"Il quadrato di {numbers[i]} è {quadrato}");
-}
+//int[] numbers = new int[insertNum];
+//for (int i = 0; i < insertNum; i++)
+//{
+//    Console.WriteLine($"Inserisci il {i + 1}° numero:");
+//    numbers[i] = int.Parse(Console.ReadLine());
+//    int quadrato = Quadrato(numbers[i]);
+//    Console.WriteLine($"Il quadrato di {numbers[i]} è {quadrato}");
+//}
 
-StampaArray(numbers);
-
-
-
-int[] squaredNumbers = ElevaArrayAlQuadrato(numbers);
-StampaArray(squaredNumbers);
-
-int somma = sommaElementiArray(numbers);
-Console.WriteLine($"La somma degli elementi del primo array è {somma}");
-
-int sommaQuadrati = sommaElementiArray(squaredNumbers);
-Console.WriteLine($"La somma dei quadrati degli elementi del secondo array è {sommaQuadrati}");
-
-void StampaArray(int[] numbers)
-{
-    Console.Write("[");
-    for (int i = 0; i < numbers.Length; i++)
-    {
-        Console.Write(numbers[i]);
-        if (i != numbers.Length - 1)
-        {
-            Console.Write(", ");
-        }
-    }
-    Console.Write("]");
-}
+//StampaArray(numbers);
 
 
-int Quadrato(int numero)
-{
-    return numero * numero;
-}
+
+//int[] squaredNumbers = ElevaArrayAlQuadrato(numbers);
+//StampaArray(squaredNumbers);
+
+//int somma = sommaElementiArray(numbers);
+//Console.WriteLine($"La somma degli elementi del primo array è {somma}");
+
+//int sommaQuadrati = sommaElementiArray(squaredNumbers);
+//Console.WriteLine($"La somma dei quadrati degli elementi del secondo array è {sommaQuadrati}");
+
+//void StampaArray(int[] numbers)
+//{
+//    Console.Write("[");
+//    for (int i = 0; i < numbers.Length; i++)
+//    {
+//        Console.Write(numbers[i]);
+//        if (i != numbers.Length - 1)
+//        {
+//            Console.Write(", ");
+//        }
+//    }
+//    Console.Write("]");
+//}
 
 
-int[] ElevaArrayAlQuadrato(int[] numbers)
-{
-    int[] newArray = new int[numbers.Length];
-    for (int i = 0; i < numbers.Length; i++)
-    {
-        newArray[i] = numbers[i] * numbers[i];
-    }
-    return newArray;
-}
+//int Quadrato(int numero)
+//{
+//    return numero * numero;
+//}
 
-int sommaElementiArray(int[] numbers)
-{
-    int somma = 0;
-    for (int i = 0; i < numbers.Length; i++)
-    {
-        somma += numbers[i];
-    }
-    return somma;
-}
+
+//int[] ElevaArrayAlQuadrato(int[] numbers)
+//{
+//    int[] newArray = new int[numbers.Length];
+//    for (int i = 0; i < numbers.Length; i++)
+//    {
+//        newArray[i] = numbers[i] * numbers[i];
+//    }
+//    return newArray;
+//}
+
+//int sommaElementiArray(int[] numbers)
+//{
+//    int somma = 0;
+//    for (int i = 0; i < numbers.Length; i++)
+//    {
+//        somma += numbers[i];
+//    }
+//    return somma;
+//}
 
 //BONUS "2
 //Creare una funzione in grado di cercare il numero più piccolo all'interno di un array di interi positivi e maggiori di 0, senza la possibilità di usare cicli e senza variabili "globali"
 
 int[] array = { 2, 6, 7, 5, 1, 9, 3};
+CheckMinNumber(array);
+
+int CheckMinNumber(int[] array)
+{
+    int sum = array[0] + array[2] + array[3] + array[4] + array[5] + array[6];
+    
+    int a = sum - array[1];
+    int b = sum - array[2];
+    int c = sum - array[3];
+    int d = sum - array[4];
+    int e = sum - array[5];
+    int f = sum - array[6];
+
+    int max = a;
+    if (b > max) max = b;
+    if (c > max) max = c;
+    if (d > max) max = d;
+    if (e > max) max = e;
+    if (f > max) max = f;
+    int numeroMinore = sum - max;
+    Console.WriteLine("il numero minore è: " + numeroMinore);
+    return numeroMinore;
+}
+

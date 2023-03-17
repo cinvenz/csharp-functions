973095a569d0931f256187c083b4d0d546e25707
cinvenz <vincenzobortune@gmail.com>
2023-03-17 14:20:53 +0100

    insert first function

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var numbers = new int[] { 2, 4, 5, 9, 3, 1, };
+
+StampaArray(numbers);
+
+void StampaArray(int[] numbers)
+{
+    foreach (int number in numbers)
+    {
+        Console.Write("elemento " + number + ", ");
+    }
+}
